2b15e7128734fc5e20575d18c94a1667fee5f6b5
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-08-22 13:27:48 -0500

    feat: :recycle: Configuracion Basica
    Cambios a la configuracion Basica

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { appToken, appVerify } from './limit/token.js';
+import storageAcudiente from './routes/acudiente.js';
+import storageCita from './routes/cita.js';
+import storageConsultorio from './routes/consultorio.js';
+import storageEspecialidad from './routes/especialidad.js';
+import storageEstado from './routes/estado_cita.js';
+import storageGenero from './routes/genero.js';
+import storageMedico from './routes/medico.js';
+import storageDocumento from './routes/tipo_documento.js';
+import storageUsuario from './routes/usuario.js'
 
-const app = express();
 dotenv.config();
-const port = process.env.port;
-const hostname = process.env.hostname
+let app = express();
 
-app.listen(port, ()=>{
-    console.log(`http://${hostname}:${port}`);
-})
+app.use("/acudiente", appVerify, storageAcudiente);
+app.use("/cita", appVerify, storageCita);
+app.use("/consultorio", appVerify, storageConsultorio);
+app.use("/especialidad", appVerify, storageEspecialidad);
+app.use("/estado", appVerify, storageEstado);
+app.use("/genero", appVerify, storageGenero);
+app.use("/medico", appVerify, storageMedico);
+app.use("/documento", appVerify, storageDocumento);
+app.use("/usuario", appVerify, storageUsuario);
+app.use("/token", appToken);
+
+
+let config = JSON.parse(process.env.MY_SERVER);
+console.log(config);
+app.listen(config, ()=>{
+    console.log(`http://${config.hostname}:${config.port}`);
+});
+
